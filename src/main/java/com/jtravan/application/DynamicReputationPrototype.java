8d7a7222ba1320bcf564f4514a62ec1424fa0d33
John <jravan@trhc.com>
2021-03-13 12:21:04 -0500

    this is it

--- src/main/java/com/jtravan/application/DynamicReputationPrototype.java
+++ src/main/java/com/jtravan/application/DynamicReputationPrototype.java
@@ -1,10 +1,8 @@
 package com.jtravan.application;
 
-import com.jtravan.components.EntryPoint;
 import lombok.extern.apachecommons.CommonsLog;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -17,12 +15,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @CommonsLog
 public class DynamicReputationPrototype {
     public static void main(String[] args) {
-        ConfigurableApplicationContext applicationContext = SpringApplication.run(DynamicReputationPrototype.class, args);
-        try {
-            applicationContext.getBean(EntryPoint.class).run();
-        } catch (InterruptedException e) {
-           log.error("Thread interrupted at Spring Boot Application");
-        }
+        SpringApplication.run(DynamicReputationPrototype.class, args);
     }
 }
 
