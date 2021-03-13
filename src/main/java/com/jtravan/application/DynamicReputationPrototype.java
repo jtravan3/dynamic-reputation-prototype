6fc38428bd635f66d6646f161bcc566848c0caaf
John <jravan@trhc.com>
2021-03-13 08:30:44 -0500

    Fixed a dumb error

--- src/main/java/com/jtravan/application/DynamicReputationPrototype.java
+++ src/main/java/com/jtravan/application/DynamicReputationPrototype.java
@@ -1,9 +1,7 @@
 package com.jtravan.application;
 
-import com.jtravan.configuration.GcpCredentials;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -11,7 +9,6 @@ import org.springframework.context.annotation.ComponentScan;
  */
 @ComponentScan(value = {"com.jtravan"})
 @SpringBootApplication
-@EnableConfigurationProperties(GcpCredentials.class)
 public class DynamicReputationPrototype {
     public static void main(String[] args) {
         SpringApplication.run(DynamicReputationPrototype.class, args);
